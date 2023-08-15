2bbbf554e3d77651d716a3797ac90735273a69d9
Leon Chou <iamyiii0328@gmail.com>
2023-08-15 14:31:36 +0800

    filter - query addition (優化後)

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -19,13 +19,67 @@ class ListingController extends Controller
      */
     public function index(Request $request)
     {
+        $filters = $request->only(['priceFrom','priceTo','beds','baths','areaFrom','areaTo']);
+        $query = Listing::mostRecent();
+
+        // * query addition 添加查詢(優化後)
+        $query->when(
+            $filters['priceFrom'] ?? false,
+            // $value 是 第一個參數為true時，第一個參數本身的值
+            fn($query,$value)=>$query->where('price', '>=',$value)
+        )->when(
+            $filters['priceTo'] ?? false,
+            fn($query,$value)=>$query->where('price', '<=',$value)
+        )->when(
+            $filters['beds'] ?? false,
+            fn($query,$value)=>$query->where('beds', (int)$value < 6 ? '=' : '>=' ,$value)
+        )->when(
+            $filters['baths'] ?? false,
+            fn($query,$value)=>$query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
+        )->when(
+            $filters['areaFrom'] ?? false,
+            fn($query,$value)=>$query->where('area', '>=',$value)
+        )->when(
+            $filters['areaTo'] ?? false,
+            fn($query,$value)=>$query->where('area', '<=',$value)
+        );
+
+        // * query addition 添加查詢(優化前)
+        // if($filters['priceFrom'] ?? false){
+        //     // 如果'priceFrom'有被定義
+        //     $query->where('price', '>=', $filters['priceFrom']);
+        // }
+
+        // if($filters['priceTo'] ?? false){
+        //     // 如果'priceTo'有被定義
+        //     $query->where('price', '<=', $filters['priceTo']);
+        // }
+
+        // if($filters['beds'] ?? false){
+        //     // 如果'beds'有被定義
+        //     $query->where('beds', $filters['beds']);
+        // }
+
+        // if($filters['baths'] ?? false){
+        //     // 如果'baths'有被定義
+        //     $query->where('baths', $filters['baths']);
+        // }
+
+        // if($filters['areaFrom'] ?? false){
+        //     // 如果'areaFrom'有被定義
+        //     $query->where('area', '>=', $filters['areaFrom']);
+        // }
+
+        // if($filters['areaTo'] ?? false){
+        //     // 如果'areaTo'有被定義
+        //     $query->where('area', '<=', $filters['areaTo']);
+        // }
+
         return inertia(
             'Listing/Index',
             [
-                'filters'=> $request->only(['priceFrom','priceTo','beds','baths','areaFrom','areaTo']),
-                'listings' => Listing::orderByDesc('created_at')
-                ->paginate(10)
-                ->withQueryString()
+                'filters'=> $filters,
+                'listings' => $query->paginate(10)->withQueryString()
             ]
         );
     }
